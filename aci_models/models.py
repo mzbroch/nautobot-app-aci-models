49340e2d5c81349e13b734431feb5c730d2f257a
Marek Zbroch <marek.zbroch@networktocode.com>
2024-06-28 14:17:06 +0200

    Model updates

--- aci_models/models.py
+++ aci_models/models.py
@@ -1,38 +1,133 @@
 """Models for Cisco ACI Models."""
 
-# Django imports
 from django.db import models
 
-# Nautobot imports
 from nautobot.apps.models import PrimaryModel
-
-# from nautobot.extras.utils import extras_features
-# If you want to use the extras_features decorator please reference the following documentation
-# https://docs.nautobot.com/projects/core/en/latest/plugins/development/#using-the-extras_features-decorator-for-graphql
-# Then based on your reading you may decide to put the following decorator before the declaration of your class
-# @extras_features("custom_fields", "custom_validators", "relationships", "graphql")
+from nautobot.core.constants import CHARFIELD_MAX_LENGTH
 
 
-# If you want to choose a specific model to overload in your class declaration, please reference the following documentation:
-# how to chose a database model: https://docs.nautobot.com/projects/core/en/stable/plugins/development/#database-models
-class ACIModel(PrimaryModel):  # pylint: disable=too-many-ancestors
-    """Base model for Cisco ACI Models app."""
-
-    name = models.CharField(max_length=100, unique=True)
-    description = models.CharField(max_length=200, blank=True)
-    # additional model fields
+class ApplicationProfile(PrimaryModel):
+    """Represents Cisco ACI Application Profile."""
+    name = models.CharField(max_length=CHARFIELD_MAX_LENGTH)
+    tenant = models.ForeignKey(
+        to="tenancy.Tenant",
+        on_delete=models.PROTECT,
+        related_name="aci_appprofiles",
+    )
+    description = models.CharField(max_length=CHARFIELD_MAX_LENGTH, blank=True)
 
     class Meta:
-        """Meta class."""
-
-        ordering = ["name"]
-
-        # Option for fixing capitalization (i.e. "Snmp" vs "SNMP")
-        # verbose_name = "Cisco ACI Models"
-
-        # Option for fixing plural name (i.e. "Chicken Tenders" vs "Chicken Tendies")
-        # verbose_name_plural = "Cisco ACI Modelss"
+        """Meta class for ApplicationProfile."""
+        verbose_name = "Cisco ACI Application Profile"
+        verbose_name_plural = "Cisco ACI Application Profiles"
+        ordering = ("name",)
+        unique_together = ("tenant", "name")
 
     def __str__(self):
-        """Stringify instance."""
+        """Return a string representation of the instance."""
         return self.name
+
+
+class BridgeDomain(PrimaryModel):
+    """Represents Cisco ACI Bridge Domain."""
+    name = models.CharField(max_length=CHARFIELD_MAX_LENGTH)
+    tenant = models.ForeignKey(
+        to="tenancy.Tenant",
+        on_delete=models.PROTECT,
+        related_name="aci_bridgedomains",
+    )
+    vrf = models.ForeignKey(
+        to="ipam.VRF",
+        related_name="aci_bridgedomains",
+        on_delete=models.PROTECT,
+    )
+    description = models.CharField(max_length=CHARFIELD_MAX_LENGTH, blank=True)
+    ip_addresses = models.ManyToManyField(
+        to="ipam.IPAddress",
+        related_name="aci_bridgedomains",
+        blank=True,
+        verbose_name="IP Addresses",
+    )
+
+    class Meta:
+        """Meta class for BridgeDomain."""
+        verbose_name = "Cisco ACI Bridge Domain"
+        verbose_name_plural = "Cisco ACI Bridge Domains"
+        ordering = ("name",)
+        unique_together = ("vrf", "name", "tenant")
+
+    def __str__(self):
+        """Return a string representation of the instance."""
+        return self.name
+
+
+class EPG(PrimaryModel):
+    """Represents Cisco ACI EPG."""
+    name = models.CharField(max_length=CHARFIELD_MAX_LENGTH)
+    tenant = models.ForeignKey(
+        to="tenancy.Tenant",
+        on_delete=models.PROTECT,
+        related_name="aci_epgs",
+    )
+    application = models.ForeignKey(
+        ApplicationProfile,
+        related_name="aci_epgs",
+        on_delete=models.CASCADE,
+    )
+    bridge_domain = models.ForeignKey(
+        BridgeDomain,
+        related_name="aci_epgs",
+        on_delete=models.CASCADE,
+    )
+    description = models.CharField(max_length=CHARFIELD_MAX_LENGTH, blank=True)
+
+    class Meta:
+        """Meta class for EPG."""
+        verbose_name = "Cisco ACI EPG"
+        verbose_name_plural = "Cisco ACI EPGs"
+        ordering = ("name",)
+        unique_together = ("name", "application", "tenant")
+
+    def __str__(self):
+        """Return a string representation of the instance."""
+        return self.name
+
+
+class ApplicationTermination(PrimaryModel):
+    """Represents Cisco ACI Application Terminations."""
+    epg = models.ForeignKey(
+        EPG,
+        related_name="aci_apptermination",
+        on_delete=models.CASCADE,
+    )
+    interface = models.ForeignKey(
+        to="dcim.Interface",
+        related_name="aci_apptermination",
+        on_delete=models.CASCADE,
+    )
+    vlan = models.ForeignKey(
+        to="ipam.VLAN",
+        related_name="aci_apptermination",
+        on_delete=models.SET_NULL,
+        blank=True,
+        null=True,
+    )
+    name = models.CharField(
+        max_length=CHARFIELD_MAX_LENGTH,
+        blank=True,
+    )
+    description = models.CharField(
+        max_length=CHARFIELD_MAX_LENGTH,
+        blank=True,
+    )
+
+    def __str__(self):
+        """Return a string representation of the instance."""
+        return f"{self.interface.device.name}:{self.interface.name}:{self.vlan.vid}"
+
+    class Meta:
+        """Meta class for ApplicationTermination."""
+        verbose_name = "Cisco ACI App Termination"
+        verbose_name_plural = "Cisco ACI App Termination"
+        ordering = ("name",)
+        unique_together = ("epg", "interface", "vlan")
