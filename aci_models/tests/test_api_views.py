1c19c093916777d0ac7a03cf13f6ef59ffcf5188
Marek Zbroch <marek.zbroch@networktocode.com>
2024-07-03 12:29:54 +0200

    daily updates

--- aci_models/tests/test_api_views.py
+++ aci_models/tests/test_api_views.py
@@ -1,6 +1,7 @@
 """Unit tests for aci_models."""
 from nautobot.apps.testing import APIViewTestCases
 
+from nautobot.ipam.models import VRF
 from nautobot.tenancy.models import Tenant
 
 from aci_models import models
@@ -17,6 +18,8 @@ class ApplicationProfileAPIViewTest(APIViewTestCases.APIViewTestCase):
     @classmethod
     def setUpTestData(cls):
         fixtures.create_tenants()
+        fixtures.create_application_profile()
+
         cls.tenant_1 = Tenant.objects.get(name=fixtures.TENANT_NAMES[0])
         cls.tenant_2 = Tenant.objects.get(name=fixtures.TENANT_NAMES[1])
 
@@ -28,6 +31,49 @@ class ApplicationProfileAPIViewTest(APIViewTestCases.APIViewTestCase):
             },
             {
                 "name": "Test Model 2",
+                "description": "test description",
                 "tenant": cls.tenant_2.pk
+            },
+            {
+                "name": "Test Model 3",
+                "tenant": cls.tenant_2.pk
+            },
+        ]
+
+
+class BridgeDomainAPIViewTest(APIViewTestCases.APIViewTestCase):
+    # pylint: disable=too-many-ancestors
+    """Test the API viewsets for BridgeDomain."""
+
+    model = models.BridgeDomain
+    bulk_update_data = {"description": "Test Bulk Update"}
+
+    @classmethod
+    def setUpTestData(cls):
+        fixtures.create_tenants()
+        fixtures.create_ipam()
+        fixtures.create_bridge_domain()
+
+        cls.tenant_1 = Tenant.objects.get(name=fixtures.TENANT_NAMES[0])
+        cls.tenant_2 = Tenant.objects.get(name=fixtures.TENANT_NAMES[1])
+        cls.vrf_1 = VRF.objects.get(name=fixtures.VRF_NAMES[0])
+
+        cls.create_data = [
+            {
+                "name": "Test Model 1",
+                "description": "test description",
+                "tenant": cls.tenant_1.pk,
+                "vrf": cls.vrf_1.pk
+            },
+            {
+                "name": "Test Model 2",
+                "description": "test description",
+                "tenant": cls.tenant_2.pk,
+                "vrf": cls.vrf_1.pk,
+            },
+            {
+                "name": "Test Model 3",
+                "tenant": cls.tenant_2.pk,
+                "vrf": cls.vrf_1.pk,
             },
         ]
