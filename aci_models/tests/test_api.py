3569e71ae7f3ac40463cc834c73cdb52f2cff759
Marek Zbroch <marek.zbroch@networktocode.com>
2024-07-03 11:52:35 +0200

    daily updates

--- aci_models/tests/test_api.py
+++ aci_models/tests/test_api.py
@@ -27,7 +27,79 @@ class ApplicationProfileAPITest(TestCase):
 
     def test_application_profile_list(self):
         """Verify that Application Profiles can be listed."""
-        url = reverse("plugins-api:aci_models:applicationprofile-list")
+        url = reverse("plugins-api:aci_models-api:applicationprofile-list")
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data["count"], len(fixtures.APP_NAMES))
+
+
+class BridgeDomainAPITest(TestCase):
+    """Test the Bridge Domain ACI Models API."""
+
+    def setUp(self):
+        """Create a superuser and token for API calls."""
+        self.user = User.objects.create(username="testuser", is_superuser=True)
+        self.token = Token.objects.create(user=self.user)
+        self.client = APIClient()
+        self.client.credentials(HTTP_AUTHORIZATION=f"Token {self.token.key}")
+
+        fixtures.create_tenants()
+        fixtures.create_ipam()
+        fixtures.create_bridge_domain()
+
+    def test_application_profile_list(self):
+        """Verify that Application Profiles can be listed."""
+        url = reverse("plugins-api:aci_models-api:bridgedomain-list")
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data["count"], len(fixtures.APP_NAMES))
+
+
+class EPGAPITest(TestCase):
+    """Test the EPG ACI Models API."""
+
+    def setUp(self):
+        """Create a superuser and token for API calls."""
+        self.user = User.objects.create(username="testuser", is_superuser=True)
+        self.token = Token.objects.create(user=self.user)
+        self.client = APIClient()
+        self.client.credentials(HTTP_AUTHORIZATION=f"Token {self.token.key}")
+
+        fixtures.create_tenants()
+        fixtures.create_ipam()
+        fixtures.create_application_profile()
+        fixtures.create_bridge_domain()
+        fixtures.create_epg()
+
+    def test_application_profile_list(self):
+        """Verify that Application Profiles can be listed."""
+        url = reverse("plugins-api:aci_models-api:epg-list")
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data["count"], len(fixtures.APP_NAMES))
+
+
+class ApplicationTerminationAPITest(TestCase):
+    """Test the ApplicationTermination ACI Models API."""
+
+    def setUp(self):
+        """Create a superuser and token for API calls."""
+        self.user = User.objects.create(username="testuser", is_superuser=True)
+        self.token = Token.objects.create(user=self.user)
+        self.client = APIClient()
+        self.client.credentials(HTTP_AUTHORIZATION=f"Token {self.token.key}")
+
+        fixtures.create_tenants()
+        fixtures.create_ipam()
+        fixtures.create_dcim()
+        fixtures.create_application_profile()
+        fixtures.create_bridge_domain()
+        fixtures.create_epg()
+        fixtures.create_application_termination()
+
+    def test_application_profile_list(self):
+        """Verify that Application Profiles can be listed."""
+        url = reverse("plugins-api:aci_models-api:applicationtermination-list")
         response = self.client.get(url)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(response.data["count"], len(fixtures.APP_NAMES))
--- aci_models/tests/test_api_views.py
+++ aci_models/tests/test_api_views.py
@@ -1,26 +1,33 @@
-# """Unit tests for aci_models."""
-# from nautobot.apps.testing import APIViewTestCases
-#
-# from aci_models import models
-# from aci_models.tests import fixtures
-#
-#
-# class ACIModelAPIViewTest(APIViewTestCases.APIViewTestCase):
-#     # pylint: disable=too-many-ancestors
-#     """Test the API viewsets for ACIModel."""
-#
-#     model = models.ACIModel
-#     create_data = [
-#         {
-#             "name": "Test Model 1",
-#             "description": "test description",
-#         },
-#         {
-#             "name": "Test Model 2",
-#         },
-#     ]
-#     bulk_update_data = {"description": "Test Bulk Update"}
-#
-#     @classmethod
-#     def setUpTestData(cls):
-#         fixtures.create_acimodel()
+"""Unit tests for aci_models."""
+from nautobot.apps.testing import APIViewTestCases
+
+from nautobot.tenancy.models import Tenant
+
+from aci_models import models
+from aci_models.tests import fixtures
+
+
+class ApplicationProfileAPIViewTest(APIViewTestCases.APIViewTestCase):
+    # pylint: disable=too-many-ancestors
+    """Test the API viewsets for ACIModel."""
+
+    model = models.ApplicationProfile
+    bulk_update_data = {"description": "Test Bulk Update"}
+
+    @classmethod
+    def setUpTestData(cls):
+        fixtures.create_tenants()
+        cls.tenant_1 = Tenant.objects.get(name=fixtures.TENANT_NAMES[0])
+        cls.tenant_2 = Tenant.objects.get(name=fixtures.TENANT_NAMES[1])
+
+        cls.create_data = [
+            {
+                "name": "Test Model 1",
+                "description": "test description",
+                "tenant": cls.tenant_1.pk
+            },
+            {
+                "name": "Test Model 2",
+                "tenant": cls.tenant_2.pk
+            },
+        ]
